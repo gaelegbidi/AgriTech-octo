dacdfb1200da3fef11560fb3952b55e490f67b40
Landry Defo Kuate <ldefokuate@octo.com>
2017-08-12 20:22:08 +0100

    Fix CORS et Ajout du token dans les appels depuis le Navigateur avec un intercepteur axios

--- src/main/java/ma/octo/agritech/controllers/UserApiController.java
+++ src/main/java/ma/octo/agritech/controllers/UserApiController.java
@@ -1,12 +1,15 @@
 package ma.octo.agritech.controllers;
 
 import ma.octo.agritech.domains.User;
+import ma.octo.agritech.domains.UserStats;
 import ma.octo.agritech.repositories.UserRepository;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -20,7 +23,7 @@ public class UserApiController {
 		this.repository = repository;
 	}
 
-	@PreAuthorize("#oauth2.hasScope('myapi:write')")
+	@PreAuthorize("#oauth2.hasScope('api:read')")
 	@RequestMapping(value = "/users/info", produces = APPLICATION_JSON_VALUE)
 	public User getUserInfo(final Principal principal) {
 
@@ -28,4 +31,17 @@ public class UserApiController {
 
 	}
 
+	@PreAuthorize("#oauth2.hasScope('api:read')")
+	@GetMapping(value = "/users/stats", produces = APPLICATION_JSON_VALUE)
+	public UserStats userCount() {
+
+		List<User> users = (List<User>) this.repository.findAll();
+		List<User> usersAcheteur = (List<User>) this.repository.findByRoles("Acheteur");
+		List<User> usersPartenaire = (List<User>) this.repository.findByRoles("Partenaire");
+		List<User> usersONG = (List<User>) this.repository.findByRoles("ONG");
+		List<User> usersPublic = (List<User>) this.repository.findByRoles("Public");
+		UserStats userStats = new UserStats(users.size(), usersAcheteur.size(), usersPartenaire.size(),
+				usersPublic.size(), usersONG.size());
+		return userStats;
+	}
 }
